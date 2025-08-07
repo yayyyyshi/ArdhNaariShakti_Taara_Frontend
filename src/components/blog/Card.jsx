2c9f58b7944258252aa94616b0a226b00b6a424f
Vidushi Agarwal <vidushiagg2004@gmail.com>
2025-08-07 21:42:36 +0530

    Update Card.jsx

--- src/components/blog/Card.jsx
+++ src/components/blog/Card.jsx
@@ -10,52 +10,36 @@ import {
 import { Link } from "react-router-dom";
 
 export const Card = ({ posts }) => {
-  // create file garnebelema
-  const PublicFlo = "https://taara-backend.onrender.com/images/";
+  const PublicFlo = "http://localhost:5000/images/"
   return (
-    <>
-      <section className="blog">
-        <div className="container grid3">
-          {posts.map((item) => (
-            <div className="box boxItems" key={item.id}>
-              {/* first ma yo  <div className='img'>{item.photo && <img src={item.cover} alt='' />}</div>*/}
-              <div className="img">
-                {item.photo && (
-                  <img src={PublicFlo + item.photo} alt={item.title} />
-                )}
+    <section className='blog'>
+      <div className='container grid3'>
+        {Array.isArray(posts) && posts.map((item) => (
+          <div className='box boxItems' key={item.id}>
+            <div className='img'>
+              {item.photo && <img src={PublicFlo + item.photo} alt='post' />}
+            </div>
+            <div className='details'>
+              <div className='tag'>
+                <AiOutlineTags className='icon' />
+                {Array.isArray(item.categories) && item.categories.map((c, index) => (
+                  <a href='/' key={index}>#{c.name}</a>
+                ))}
               </div>
-              <div className="details">
-                <div className="tag">
-                  <AiOutlineTags className="icon" />
-                  {item.categories.map((c, index) => (
-                    <a href="/" key={index}>
-                      #{c.name}
-                    </a>
-                  ))}
-                </div>
-                <Link to={`/post/${item._id}`}>
-                  <h3>{item.title}</h3>
-                </Link>
-                <p>{item.desc.slice(0, 150)}...</p>
-                <div className="date">
-                  <div>
-                    <AiOutlineClockCircle className="icon" />
-                    <label htmlFor="">
-                      {new Date(item.createdAt).toDateString()}
-                    </label>
-                  </div>
-                  <div>
-                    <AiOutlineComment className="icon" />
-                    <label htmlFor="">27</label>
-                    <AiOutlineShareAlt className="icon" />
-                    <label htmlFor="">SHARE</label>
-                  </div>
-                </div>
+              <Link to={`/post/${item._id}`}>
+                <h3>{item.title || "Untitled Post"}</h3>
+              </Link>
+              <p>{item.desc ? item.desc.slice(0, 180) + "..." : "No description available."}</p>
+              <div className='date'>
+                <AiOutlineClockCircle className='icon' />
+                <label>{item.createdAt ? new Date(item.createdAt).toDateString() : "Date not available"}</label>
+                <AiOutlineComment className='icon' /> <label>27</label>
+                <AiOutlineShareAlt className='icon' /> <label>SHARE</label>
               </div>
             </div>
-          ))}
-        </div>
-      </section>
-    </>
-  );
-};
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
